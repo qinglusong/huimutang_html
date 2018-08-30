5c77df88f216d77f0d0535ed6c87fdef5f788308
zhiyu4 <zhiyu4@z4iMac.local>
2018-08-30 10:08:44 +0800

    zhiyu

---
+++ js/tool.js
@@ -0,0 +1,16 @@
+//
+var titlebanner=new Swiper('#consult_sp1', {
+    initialSlide :0,
+    speed:1000,
+    // autoplay: {
+    //     delay: 5000,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: true,
+    // },
+
+    on: {
+        slideChangeTransitionEnd: function(){
+
+            }
+        },
+});
